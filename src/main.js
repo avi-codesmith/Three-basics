268c963b6683614971484f47214960f1540773cd
avi-codesmith <avi.codesmith@gmail.com>
2025-06-03 13:44:03 +0530

    Add Orbit controls

--- src/main.js
+++ src/main.js
@@ -30,12 +30,13 @@ window.addEventListener("resize", () => {
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.OrbitControls = true;
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.dampingFactor = 0.01;
 
 function animate() {
   requestAnimationFrame(animate);
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
   controls.update();
 }
